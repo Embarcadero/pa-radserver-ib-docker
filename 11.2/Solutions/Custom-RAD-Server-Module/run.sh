582c5634f5903a81a7510c08cd578b888d36d743
Serhii Kheilyk <1171495+lynxnake@users.noreply.github.com>
2023-01-12 12:16:12 +0200

    Update to 11.2

--- 11.2/Solutions/Custom-RAD-Server-Module/run.sh
+++ 11.2/Solutions/Custom-RAD-Server-Module/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 echo "PAServer Password: securepass"
-docker run -it -e PA_SERVER_PASSWORD=securepass -p 80:80 -p 64211:64211 -p 8082:8082 -p 3050:3050 --mount source=interbase,target=/opt/interbase --mount source=ems,target=/etc/ems pa-radserver-ib-custom-module
+docker run -it -e PA_SERVER_PASSWORD=securepass \
+    --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+    -p 80:80 -p 64211:64211 -p 8082:8082 -p 3050:3050 \
+    --mount source=interbase,target=/opt/interbase \
+    --mount source=ems,target=/etc/ems \
+    pa-radserver-ib-custom-module
--- 11.2/build.sh
+++ 11.2/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build . --build-arg password=securepass
+docker build . --build-arg password=securepass \
+    --tag=radstudio/pa-radserver-ib:11.2 --tag=radstudio/pa-radserver-ib:alexandria \
+    --tag=radstudio/pa-radserver-ib:latest
