e8c6880ccd0b24b8a904552cd6debdc684f40795
Serhii Kheilyk <1171495+lynxnake@users.noreply.github.com>
2023-02-26 22:04:09 +0200

    Specify tags in 10.4.1

--- 10/10.4.1/Solutions/Custom-RAD-Server-Module/build.sh
+++ 10/10.4.1/Solutions/Custom-RAD-Server-Module/build.sh
@@ -4,10 +4,11 @@ if [ "$1" == '' ]; then
     echo "RAD Server Docker pa-radserver-ib-custom-module Build Script";
     echo "Place RAD Server endpoint custom resource module in this directory and pass it as the first argument.";
     echo "Required argument: module filename";
-    echo "ex: build.sh module.so";
+    echo "ex: build.sh bplProject1.so";
 else
-    if [ -e ./$1]; then
-        docker build . --build-arg password=securepass --build-arg ModuleFile=$1 -t pa-radserver-ib-custom-module
+    if [ -e ./$1 ]; then
+        docker build . --build-arg password=securepass --build-arg modulefile=$1 \
+            -t pa-radserver-ib-custom-module:10.4.1 --no-cache
     else
         echo "The RAD Server endpoint custom resource module needs to be in this directory.";
     fi
