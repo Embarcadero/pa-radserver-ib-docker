3bbb213828fe8b6f2fdf6b18dc434cf5a3e6fedb
Jim McKeeth <jim@mckeeth.org>
2023-03-01 12:19:03 -0700

    Fixed revert

--- 10/10.3.3/config.sh
+++ 10/10.3.3/config.sh
@@ -3,7 +3,7 @@
 if [ "$1" == '' ]; then
     echo "RAD Server Docker pa-radserver-ib Configure Script";
     echo "Required arguments: container hash or name";
-    echo "ex: config.sh <TODO>";
+    echo "ex: config.sh gracious_galois";
 else
     docker exec -it $1 sh -c "apt-get update && apt-get install -y nano ; nano /etc/ems/emsserver.ini; service apache2 restart"
 fi
