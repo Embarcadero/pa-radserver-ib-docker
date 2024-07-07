ba6af7515c6d67413fde14b3a059b4e6d690e4ec
lmbelo <lucas.belo@live.com>
2024-07-07 18:42:25 -0300

    Drop broadway support

--- Solutions/Custom-RAD-Server-Module/run.sh
+++ Solutions/Custom-RAD-Server-Module/run.sh
@@ -3,7 +3,7 @@
 echo "PAServer Password: securepass"
 docker run -it -e PA_SERVER_PASSWORD=securepass \
     --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-    -p 80:80 -p 64211:64211 -p 8082:8082 -p 3050:3050 \
+    -p 80:80 -p 64211:64211 -p 3050:3050 \
     --mount source=interbase,target=/opt/interbase \
     --mount source=ems,target=/etc/ems \
     pa-radserver-ib-custom-module
--- run-production.sh
+++ run-production.sh
@@ -2,7 +2,7 @@
 
 echo "PAServer Password: securepass"
 docker run  -it -e PA_SERVER_PASSWORD=securepass \
-    -p 80:80 -p 64211:64211 -p 8082:8082 -p 3050:3050 \
+    -p 80:80 -p 64211:64211 -p 3050:3050 \
     --mount source=interbase,target=/opt/interbase \
     --mount source=ems,target=/etc/ems  \
     --platform linux/amd64 \
